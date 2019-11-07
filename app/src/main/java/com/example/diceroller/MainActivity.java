2d11d4833258e0b18246a9b768fa48ae68d58e4e
aliyu <57145780+aliyukabir@users.noreply.github.com>
2019-11-07 23:01:44 +0000

    COMPLETED DICEROLLER PROJECT

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diceroller;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,15 +9,20 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int count = 0;
+    static ArrayList<String> alist = new ArrayList<>();
+    public static final String EXTRA_MESSAGE = "Hi";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +38,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+        alist.add("if you could go anywhere in the world, where would you go");
+        alist.add("if you were stranded on a desert island, what three things would you want to take with you");
+        alist.add("if you could eat only one food for the rest of your life, what would that be?");
+        alist.add("if you won a million dollars, what is the first thing you would buy?");
+        alist.add("if you could spend the day with one fictional character, who would it be?");
+        alist.add("if you found a magic lantern and a genie gave you three wishes, what would you wish");
 
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent (this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        startActivity(intent);
+    }
+    /*public void sendMessage2(View view){
+        Intent intent2 = new Intent (this, Main3Activity.class);
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        String message = tv3.getText(). toString();
+        intent2.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent2);
+
+    }*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -55,14 +80,50 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void roll_the_dice(View view){
+        EditText et = this.findViewById(R.id.editText);
+        TextView tv = this.findViewById(R.id.textView);
+        TextView tv2 = this.findViewById(R.id.textView);
+        try{
+            int number = Random(  7 - 1) + 1;
 
-    public void on_button_click(View view){
+            tv.setText(Integer.toString(number));
+            Integer number2 = Integer.valueOf(et.getText().toString());
 
-       TextView tv = this.findViewById(R.id.textView2);
+            if (number2 == number){
+                tv2.setVisibility(View.VISIBLE);
+                counting();
 
-       Random r = new Random();
-       int number = r.nextInt(6);
+            }
+            else if (number > 6) {
+                tv2.setText("Invalid entry");
 
-       tv.setText(Integer.toString(number));
+            }else {
+                tv2.setVisibility(View.INVISIBLE);
+
+            }
+
+        }catch (Exception ex) {
+            Log.e("MyErrors", ex.toString());
+        }
+
     }
+    public void D_ICEBRAKERS(View view){
+        TextView tv4 = this.findViewById(R.id.textView);
+        int number3 = Random(alist.size());
+        tv4.setText(alist.get(number3));
+
+    }
+    public int Random (int n){
+        Random r = new Random();
+        int ran = r.nextInt (n);
+        return ran;
+    }
+    public void counting(){
+        TextView tv2 = this.findViewById(R.id.textView);
+        count++;
+        tv2.setText(Integer.toString(count));
+    }
+
+
 }
